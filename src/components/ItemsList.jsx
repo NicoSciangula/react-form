966ec5627b118f2600035c9b03e22eaa209a7acd
NicoSciangula <nico.sciangula99@gmail.com>
2026-02-23 16:17:27 +0100

    added button to remove element

--- src/components/ItemsList.jsx
+++ src/components/ItemsList.jsx
@@ -1,9 +1,17 @@
-export default function ItemsList({ item }) {
+export default function ItemsList({ item, remove }) {
   return (
     <ul>
       {item.map((singleItem, index) => (
         <li key={index} className="fs-4">
           {singleItem}
+          <button
+            onClick={() => {
+              remove(index);
+            }}
+            className="btn btn-danger ms-3"
+          >
+            &#10008;
+          </button>
         </li>
       ))}
     </ul>
