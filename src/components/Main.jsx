69bde146f2a48c7e475e57b1af6a69264f260685
NicoSciangula <nico.sciangula99@gmail.com>
2026-02-23 15:56:44 +0100

    added the two states in the parent, and added function to add tasks

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,16 +3,23 @@ import AddItem from "./AddItem";
 import { useState } from "react";
 
 export default function Main() {
-  const [item, setItem] = useState(["Pane", "Pesce", "Pasta"]);
-  const [newItem, setNewItem] = useState("");
+  const [tasks, setTasks] = useState(["Pane", "Pesce", "Pasta"]);
+  const [newTask, setNewTask] = useState("");
+
+  function addTask(event) {
+    event.preventDefault();
+    if (newTask.trim() === "") return;
+    setTasks([...tasks, newTask]);
+    setNewTask("");
+  }
 
   return (
     <>
       <main className="container">
         <div className="p-2 d-flex flex-column align-items-center">
           <h1 className="text-center p-3">Lista Articoli</h1>
-          <ItemsList />
-          <AddItem />
+          <ItemsList item={tasks} />
+          <AddItem addItems={addTask} item={newTask} setNewItem={setNewTask} />
         </div>
       </main>
     </>
