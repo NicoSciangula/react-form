ce1385829e2a4127f60b964e8cd2ab608d15e069
NicoSciangula <nico.sciangula99@gmail.com>
2026-02-23 15:05:28 +0100

    create component form

---
+++ src/components/AddItem.jsx
@@ -0,0 +1,12 @@
+export default function AddItem() {
+  return (
+    <>
+      <form action="submite">
+        <input type="text" />
+      </form>
+      <button type="submite" className="btn btn-primary mt-3">
+        Aggiungi
+      </button>
+    </>
+  );
+}
