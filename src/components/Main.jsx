e9290706960d5748c96cfad627e09e1a8069f0ca
NicoSciangula <nico.sciangula99@gmail.com>
2026-02-23 15:31:18 +0100

    imported useState

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,11 @@
 import ItemsList from "./ItemsList";
 import AddItem from "./AddItem";
+import { useState } from "react";
 
 export default function Main() {
+  const [item, setItem] = useState(["Pane", "Pesce", "Pasta"]);
+  const [newItem, setNewItem] = useState("");
+
   return (
     <>
       <main className="container">
